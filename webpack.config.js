1ba29b68c750690a212f94e47581bd8594d43fc6
kmjyz44 <kmjyz44@gmail.com>
2022-07-03 13:42:57 +0300

    fix webpack, Eslit

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
             {
-                test: /\.css$/i,
+                test: /\.css$/i, 
                 use: ['style-loader', 'css-loader'],
+                test: /\.ts$/i,
+                use: 'ts-loader' 
             },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
@@ -26,6 +28,8 @@ const baseConfig = {
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
+        new EslingPlugin({ extensions: 'ts' }),
+
         new CleanWebpackPlugin(),
     ],
 };
