90f35489cd520a08e68dfb549b125243c373b853
kmjyz44 <kmjyz44@gmail.com>
2022-07-03 14:58:11 +0300

    fix source

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -1,9 +1,19 @@
-export interface Sources {
+export interface ISources {
 id:string,
 name:string,
 description:string,
 url:string,
 category:string,
 language:string,
-country:string
+country:string,
+data:[],
+status:string,
+sources:[],
+author:string,
+publishedAt:string,
+urlToImage:string,
+title:string,
+source:[],
+
+
 }
